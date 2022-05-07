385452384b0dead5041e48d0d829a691b2da1a81
Luke <lukewberg@gmail.com>
2022-05-07 12:58:31 -0700

    Add project files.

---
+++ API-lukewberg.dev/Controllers/ArticleController.cs
@@ -0,0 +1,29 @@
+﻿using API_lukewberg.dev.Models;
+using API_lukewberg.dev.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace API_lukewberg.dev.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase {
+
+        private MongoService _mongoService;
+        public ArticleController(MongoService mongoService)
+        {
+            _mongoService = mongoService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Article> Get()
+        {
+            var collection = _mongoService.Database.GetCollection<Article>("articles");
+            List<Article> result = collection.Find(new BsonDocument()).SortByDescending(article => article.TimeStamp).Limit(5).ToList();
+
+            return result;
+        }
+    }
+}
---
+++ API-lukewberg.dev/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace API_lukewberg.dev.Models
+{
+    public class Article
+    {
+        [BsonId]
+        public ObjectId ArticleId { get; set; }
+        [BsonElement("title")]
+        public string Title { get; set; }
+
+        [BsonElement("body")]
+        public string Body { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        [BsonRepresentation(BsonType.DateTime)]
+        [BsonElement("timestamp")]
+        public DateTime TimeStamp { get; set; }
+
+    }
+}
